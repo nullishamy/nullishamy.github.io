018487fc823996a75d62beb6909dd0dfa8e5f8b2
nullishamy <git@amyerskine.me>
2024-05-05 21:20:25 +0100

    feat: mobile support

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import ExternalRead from '../ExternalRead.svelte';
 	import WorkingOnCard from '../WorkingOnCard.svelte';
-  import { externalReads, posts } from '../data'
+	import { externalReads, posts } from '../data';
 </script>
 
 <svelte:head>
@@ -14,14 +14,19 @@
 	<meta name="twitter:card" content="summary_large_image" />
 </svelte:head>
 
-<div class="flex flex-row mx-6 gap-6">
+<div class="flex flex-col lg:flex-row mx-6 gap-1 lg:gap-6">
 	<header class="flex flex-col basis-5/6 gap-8">
 		<h1
-			class="text-7xl mt-4 font-title bg-clip-text text-transparent py-3 bg-gradient-to-r from-maroon to-pink"
+			class="
+        text-5xl text-center lg:text-left lg:text-7xl mt-4 font-title bg-clip-text
+        text-transparent py-3 bg-gradient-to-r from-maroon to-pink
+      "
 		>
 			Hello, I’m Amy
 		</h1>
-		<section class="bg-mantle p-3 my-4 rounded-md mt-auto font-mono drop-shadow-md">
+		<section
+			class="bg-mantle text-sm lg:text-lg p-3 my-4 rounded-md mt-auto font-mono drop-shadow-md"
+		>
 			<p>A hobbyist software developer, based in the UK.</p>
 			<br />
 			<p>
@@ -44,27 +49,33 @@
 	</header>
 
 	<section class="bg-mantle p-2 my-4 rounded-md basis-2/6 font-mono drop-shadow-md">
-		<h2 class="text-center text-xl">You can find me in other places:</h2>
+		<h2 class="text-center text-md lg:text-xl min-h-32">You can find me in other places:</h2>
 	</section>
 </div>
 
-<main class="flex flex-row mt-8 gap-16 mx-6">
-	<section class="flex flex-col w-1/2 basis-2/3 ">
-		<h2 class="text-2xl m-6 underline self-center font-medium font-mono">
-			Here's what I've been up to lately
+<main class="flex flex-col lg:flex-row mt-8 gap-6 mx-3 lg:mx-6 mb-2">
+	<section class="flex flex-col w-full lg:w-1/2 basis-2/3">
+		<h2 class="text-lg lg:text-2xl my-2 lg:my-6 underline self-center lg:self-baseline font-medium font-mono">
+			What I've been up to lately
 		</h2>
 
-		<div class="grid grid-cols-2 items-center justify-items-center grid-rows-3 gap-6 drop-shadow-md">
+		<div
+			class="grid grid-cols-1 gap-6 lg:grid-cols-2 lg:grid-rows-3 lg:gap-6 items-center justify-items-center drop-shadow-md"
+		>
 			{#each posts as post}
 				<WorkingOnCard {post} />
 			{/each}
 		</div>
 	</section>
 
-	<section class="flex flex-col w-1/2 basis-1/3 font-mono">
-		<h2 class="text-2xl m-6 underline self-center font-medium">Interesting reads I've found</h2>
+	<section class="flex flex-col w-full lg:w-1/2 basis-2/3">
+		<h2 class="text-lg lg:text-2xl my-2 lg:my-6 underline self-center lg:self-end font-medium font-mono">
+			Interesting reads I've found
+		</h2>
 
-		<div class="grid grid-cols-1 items-center justify-items-center grid-rows-3 gap-6 drop-shadow-md">
+		<div
+			class="grid grid-cols-1 items-center justify-items-center grid-rows-3 gap-6 drop-shadow-md"
+		>
 			{#each externalReads as post}
 				<ExternalRead {post} />
 			{/each}
