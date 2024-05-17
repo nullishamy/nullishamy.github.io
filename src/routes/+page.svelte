f8c508b9ef4365e070821f567e720e8599f3180e
nullishamy <git@amyerskine.me>
2024-05-17 01:18:30 +0100

    feat: all blog posts; navigation improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
 	import { onMount } from 'svelte';
 	import ContactCardBack from '$lib/components/ContactCardBack.svelte';
 	import ContactCardFront from '$lib/components/ContactCardFront.svelte';
+	import MediaQuery from '$lib/components/MediaQuery.svelte';
 
 	const refreshTheme = () => {
 		if (
@@ -37,6 +38,11 @@
 	const handleContactFlip = () => {
 		contactFlipped = !contactFlipped;
 	};
+
+	let hamburgerOpen = false;
+	const handleHamburger = () => {
+		hamburgerOpen = !hamburgerOpen;
+	};
 </script>
 
 <svelte:head>
@@ -59,34 +65,81 @@
 >
 	<h1
 		class="
-			text-5xl text-center md:text-left md:text-6xl lg:text-7xl font-header bg-clip-text
-			text-transparent py-3 bg-gradient-to-r from-yellow via-peach to-pink lg:col-span-3
-			md:col-span-3
-			bg-300% animate-gradient
-		"
+      text-5xl md:text-left md:text-6xl lg:text-7xl font-header bg-clip-text
+      text-transparent py-3 bg-gradient-to-r from-yellow via-peach to-pink lg:col-span-3
+      md:col-span-3
+      bg-300% animate-gradient
+    "
 	>
 		hello, i’m amy
 	</h1>
 
-	<div
-		class="md:col-start-4 lg:col-start-4 lg:row-start-1 font-inclusive place-self-center flex flex-row lg:flex-col lg:gap-2 xl:flex-row xl:gap-6 gap-8 items-center"
-	>
-		<div class="flex flex-row gap-8">
-			<a href="https://ctp-webr.ing/amy/previous"><Icon width="30" icon="ph:caret-double-left" /></a
+	<MediaQuery query="(max-width: 767px)" let:matches>
+		{#if matches}
+			<button class="absolute top-9 right-5" on:click={handleHamburger}>
+				<Icon width="30" icon="ph:list" />
+			</button>
+			{#if hamburgerOpen}
+				<div
+					class="
+            absolute top-9 right-14 bg-mantle p-4 rounded-md drop-shadow-md
+            grid grid-cols-4 grid-rows-2 gap-6 z-10
+          "
+				>
+					<a title="Webring previous" href="https://ctp-webr.ing/amy/previous"
+						><Icon width="25" icon="ph:caret-double-left" /></a
+					>
+					<a title="Webring info" target="_blank" href="https://ctp-webr.ing/"
+						><Icon width="25" icon="ph:fediverse-logo" /></a
+					>
+					<a title="Webring next" href="https://ctp-webr.ing/amy/next"
+						><Icon width="25" icon="ph:caret-double-right" /></a
+					>
+					<button title="Theme toggle" on:click={handleThemeChange}
+						><Icon width="25" icon="ph:lightbulb" /></button
+					>
+
+					<button title="Undecided content"><Icon width="25" icon="ph:seal-question" /></button>
+					<button title="Undecided content"><Icon width="25" icon="ph:seal-question" /></button>
+					<button title="Undecided content"><Icon width="25" icon="ph:seal-question" /></button>
+					<a title="All blog posts" href="/post/list"><Icon width="25" icon="ph:notepad" /></a>
+				</div>
+			{/if}
+		{:else}
+			<div
+				class="
+          col-start-4 row-start-1 font-inclusive place-self-center
+          grid grid-cols-4 grid-rows-2 gap-6
+        "
 			>
-			<a target="_blank" href="https://ctp-webr.ing/"><Icon width="30" icon="ph:fediverse-logo" /></a>
-			<a href="https://ctp-webr.ing/amy/next"><Icon width="30" icon="ph:caret-double-right" /></a>
-		</div>
-		<button on:click={handleThemeChange}><Icon width="30" icon="ph:lightbulb" /></button>
-	</div>
+				<a title="Webring previous" href="https://ctp-webr.ing/amy/previous"
+					><Icon width="30" icon="ph:caret-double-left" /></a
+				>
+				<a title="Webring info" target="_blank" href="https://ctp-webr.ing/"
+					><Icon width="30" icon="ph:fediverse-logo" /></a
+				>
+				<a title="Webring next" href="https://ctp-webr.ing/amy/next"
+					><Icon width="30" icon="ph:caret-double-right" /></a
+				>
+				<button title="Theme toggle" on:click={handleThemeChange}
+					><Icon width="30" icon="ph:lightbulb" /></button
+				>
+
+				<button title="Undecided content"><Icon width="30" icon="ph:seal-question" /></button>
+				<button title="Undecided content"><Icon width="30" icon="ph:seal-question" /></button>
+				<button title="Undecided content"><Icon width="30" icon="ph:seal-question" /></button>
+				<a title="All blog posts" href="/post/list"><Icon width="30" icon="ph:notepad" /></a>
+			</div>
+		{/if}
+	</MediaQuery>
 
 	<section
 		class="
-			bg-mantle text-sm py-4 lg:text-lg px-4
-			rounded-md md:h-full font-inclusive drop-shadow-md md:col-start-1 lg:row-start-2 lg:col-span-4 md:pb-16 md:pt-4
-			md:col-span-2
-			outline outline-maroon outline-1
-		"
+      bg-mantle text-sm py-4 lg:text-lg px-4
+      rounded-md md:h-full font-inclusive drop-shadow-md md:col-start-1 lg:row-start-2 lg:col-span-4 md:pb-16 md:pt-4
+      md:col-span-2
+      outline outline-maroon outline-1
+    "
 	>
 		<p>A hobbyist software developer, based in the UK.</p>
 		<br />
@@ -110,25 +163,24 @@
 
 	<div
 		class="
-			md:col-start-3 md:col-span-2 lg:col-start-5 lg:col-span-2 lg:row-span-2
-			bg-mantle p-2 rounded-md font-inclusive drop-shadow-md outline outline-1 outline-pink
-			min-h-[21rem] md:min-h-[26rem] h-full relative
-		"
+      md:col-start-3 md:col-span-2 lg:col-start-5 lg:col-span-2 lg:row-span-2
+      bg-mantle p-2 rounded-md font-inclusive drop-shadow-md outline outline-1 outline-pink
+      min-h-[21rem] md:min-h-[26rem] h-full relative
+    "
 	>
 		{#if contactFlipped}
 			<ContactCardBack />
 		{:else}
 			<ContactCardFront />
-
-			{/if}
-			<button
-				on:click={handleContactFlip}
-				class="bg-crust w-full h-10 grid grid-cols-3 items-center justify-items-center rounded-md absolute bottom-0 left-0"
-			>
-				<Icon icon="ph:arrow-up-right" />
-				<Icon icon="ph:arrow-up-right" />
-				<Icon icon="ph:arrow-up-right" />
-			</button>
+		{/if}
+		<button
+			on:click={handleContactFlip}
+			class="bg-crust w-full h-10 grid grid-cols-3 items-center justify-items-center rounded-md absolute bottom-0 left-0"
+		>
+			<Icon icon="ph:arrow-up-right" />
+			<Icon icon="ph:arrow-up-right" />
+			<Icon icon="ph:arrow-up-right" />
+		</button>
 	</div>
 </header>
 
--- src/routes/post/list/+page.svelte
+++ src/routes/post/list/+page.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
-	import type { Post } from '$lib/types';
-
-	export let data: { posts: Post[]; query: string };
+	import { posts } from '../../../content';
 </script>
 
 <section class="flex flex-col items-center justify-items-center gap-8 m-4 mb-36">
 	<div class="grid grid-cols-4 md:grid-cols-5 items-center justify-items-center">
 		<a href="/" aria-label="Go to the home page"><Icon icon="ph:house" width="35" height="35" /></a>
 		<h1 class="text-3xl lg:text-5xl font-mono col-span-3 row-span-2 md:row-span-1 text-center">
-			Posts with tag '{data.query}'
+			All posts
 		</h1>
 	</div>
 
-	{#each data.posts as post}
+	{#each posts as post}
 		<a href="/post/{post.slug}" class="rounded-md p-2 m-2 bg-base shadow-md grid grid-cols-4 gap-y-2 font-inclusive w-full md:w-3/5 lg:w-2/5">
       <p class="text-subtext1 text-lg row-span-2">{post.publishDate.toLocaleDateString()}</p>
       <h2 class="text-lg col-start-3 col-span-2 text-right">{post.title}</h2>
